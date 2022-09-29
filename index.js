7c0b7ac4da00f7644b61daee12a0d26d874f383c
shad-crypto <travisislit2345@gmail.com>
2022-09-29 02:52:57 -0400

    updated countdown to include minutes. Cleaned up code.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const eventRetrieveal = () => {
-  const millisecondsInDay = 86400000;
-  const millisecondsIn7H = 25200000;
-  const millisecondsin1H = 3600000;
+ 
+  const millisecondsInDay = 86400000,
+    millisecondsIn7H = 25200000,
+    millisecondsin1H = 3600000,
+    millisecondsinMin = 60000;
+
   const now = new Date();
   //no value edge case
   let nearestEvent = null;
@@ -16,11 +19,8 @@ const eventRetrieveal = () => {
     }
   }
 
-  const diffInMs = (new Date(nearestEvent["dtstart"]) - now) 
+  const diffInMs = new Date(nearestEvent["dtstart"]) - now;
 
-  const numDays =
-    Math.floor((new Date(nearestEvent["dtstart"]) - now) / millisecondsInDay) +
-    1;
   document.getElementById("title").innerHTML = `${nearestEvent["summary"]}`;
   document.getElementById("date").innerHTML = `${new Intl.DateTimeFormat(
     "en-US",
@@ -30,25 +30,48 @@ const eventRetrieveal = () => {
     }
   ).format(new Date(nearestEvent["dtstart"]))}`;
 
- const numHours = Math.floor(diffInMs / millisecondsin1H)
-  if (diffInMs < millisecondsIn7H) {
-    if ((numHours + 1) > 1)  {
-      document.getElementById("countdown").innerHTML = `Starts in ${numHours + 1} hours`
-      document.getElementById("countdown-mobile").innerHTML = `Starts in ${numHours + 1} hours`
-    }
-    else{
-      document.getElementById("countdown").innerHTML = `Starts in ${numHours + 1} hour`
-      document.getElementById("countdown-mobile").innerHTML = `Starts in ${numHours + 1} hour`
-    }
-  }
+  const numDays =
+    Math.floor((new Date(nearestEvent["dtstart"]) - now) / millisecondsInDay) +
+    1;
+  const numHours = Math.floor(diffInMs / millisecondsin1H) + 1;
+  const numMins = Math.floor(diffInMs / millisecondsinMin) + 1;
 
-  else{
+  if (diffInMs < millisecondsIn7H && diffInMs > millisecondsin1H) {
+    if (numHours > 1) {
+      document.getElementById(
+        "countdown"
+      ).innerHTML = `Starts in ${numHours} hours`;
+      document.getElementById(
+        "countdown-mobile"
+      ).innerHTML = `Starts in ${numHours} hours`;
+    } else {
+      document.getElementById(
+        "countdown"
+      ).innerHTML = `Starts in ${numHours} hour`;
+      document.getElementById(
+        "countdown-mobile"
+      ).innerHTML = `Starts in ${numHours} hour`;
+    }
+  } else if (diffInMs < millisecondsin1H) {
+    if (numMins > 1) {
+      document.getElementById(
+        "countdown"
+      ).innerHTML = `Starts in ${numMins} minutes`;
+
+      document.getElementById(
+        "countdown-mobile"
+      ).innerHTML = `Starts in ${numMins} minutes`;
+    } else {
+      document.getElementById("countdown").innerHTML = "Starts in 1 minute";
+      document.getElementById("countdown-mobile").innerHTML =
+        "Starts in 1 minute";
+    }
+  } else {
     document.getElementById("countdown").innerHTML =
-    `Starts in ${numDays} ` + (numDays > 1 ? "days" : "day");
+      `Starts in ${numDays} ` + (numDays > 1 ? "days" : "day");
     document.getElementById("countdown-mobile").innerHTML =
-    `Starts in ${numDays} ` + (numDays > 1 ? "days" : "day");
+      `Starts in ${numDays} ` + (numDays > 1 ? "days" : "day");
   }
-  
 };
 
 window.onload = (_evt) => {
