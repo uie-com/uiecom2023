7c7bc531903efc8790025a2ec85a616469607271
Henry_DelGlitch <CodingWHenry@gmail.com>
2022-11-02 11:50:37 -0400

    Fetched data from airtable and converted the event to have our nearest event.

--- index.js
+++ index.js
@@ -7,7 +7,18 @@
 
 //Issues
 //Current events are one hour ahead
-const eventRetrieveal = () => {
+import { AIRTABLE_API_KEY, AIRTABLE_BASE } from './secret.js'
+
+async function eventRetrieveal() {
+
+  let airtableData = [];
+
+  let response = await fetch(`https://api.airtable.com/v0/${AIRTABLE_BASE}/Events?api_key=${AIRTABLE_API_KEY}`);
+  let text = await response.text();
+
+  if (text.endsWith(",")) text = text.slice(0, -1);
+
+  airtableData = JSON.parse(text);
 
   const millisecondsInDay = 86400000,
     millisecondsIn7H = 25200000,
@@ -15,45 +26,43 @@ const eventRetrieveal = () => {
     millisecondsinMin = 60000;
 
   const now = new Date();
-  //no value edge case
+  //no value edge cases
   let nearestEvent = null;
 
   //Grabbing the events from calendar.js
-  for (const currEvent of airtableData) {
+  for (let i = 0; i < airtableData.records.length; i++) {
     //Checks if the event has ended already
-    if (Date.parse(currEvent["dtend"]) > now) {
+    if (Date.parse(airtableData.records[i].fields["Start Date"]) > now) {
       //Checks if the next event start is greater than the current event
-      if (
-        nearestEvent == null ||
-        Date.parse(nearestEvent["dtstart"]) > Date.parse(currEvent["dtstart"])
-      ) {
-        //If our current event has ended and our next event start date is close then it will 
-        // be assigned as our current event
-        nearestEvent = currEvent;
-      }
+      nearestEvent = airtableData.records[i];
     }
   }
 
-  const timeTillStart = new Date(nearestEvent["dtstart"]) - now,
+  const timeTillStart = new Date(nearestEvent.fields["Start Date"]) - now,
     dateOptions = {
       dateStyle: "full",
       timeStyle: "short",
     };
 
-  document.getElementById("title").innerHTML = `${nearestEvent["summary"]}`;
+  document.getElementById("title").innerHTML = `${nearestEvent.fields["Title"]}`;
   document.getElementById("date").innerHTML = `${new Intl.DateTimeFormat(
     "en-US",
     dateOptions
-  ).format(new Date(nearestEvent["dtstart"]))}`;
-
+  ).format(new Date(nearestEvent.fields["Start Date"]))}`;
+  document.getElementById("inner-text").innerHTML = `${nearestEvent.fields["Event Summary"]}`
   //Getting the year, month, day, hour for the nearest event
-  let eventStartFullYear = new Date(nearestEvent["dtstart"]).getFullYear();
-  let eventStartMonth = parseInt(new Date(nearestEvent["dtstart"]).getMonth()) + 1;
-  let eventStartDay = new Date(nearestEvent["dtstart"]).getDate();
-  let eventStartHour = parseInt(new Date(nearestEvent["dtstart"]).getHours()) + 4;
-  let eventEndHour = parseInt(new Date(nearestEvent["dtend"]).getHours()) + 4;
+  let eventStartFullYear = new Date(nearestEvent.fields["Start Date"]).getFullYear();
+  let eventStartMonth = parseInt(new Date(nearestEvent.fields["Start Date"]).getMonth()) + 1;
+  let eventStartDay = new Date(nearestEvent.fields["Start Date"]).getDate();
+  if (eventStartDay < 10) {
+    eventStartDay = `0` + new Date(nearestEvent.fields["Start Date"]).getDate();
+  } else {
+    new Date(nearestEvent.fields["Start Date"]).getDate();
+  }
+  let eventStartHour = parseInt(new Date(nearestEvent.fields["Start Date"]).getHours()) + 5;
+  let eventEndHour = parseInt(new Date(nearestEvent.fields["End Date"]).getHours()) + 5;
 
-  document.getElementById("google-calendar").href = `https://calendar.google.com/calendar/u/0/r/eventedit?text=${nearestEvent["summary"]}&dates=${eventStartFullYear}${eventStartMonth}${eventStartDay}T${eventStartHour}0000Z/${eventStartFullYear}${eventStartMonth}${eventStartDay}T${eventEndHour}0000Z&details=https://us02web.zoom.us/j/88292728501?pwd%3DY0tBR25GbFF4MUxhWnd5MFI5S3hNUT09&location=https://us02web.zoom.us/j/88292728501?pwd=Y0tBR25GbFF4MUxhWnd5MFI5S3hNUT09&trp=true`
+  document.getElementById("google-calendar").href = `https://calendar.google.com/calendar/u/0/r/eventedit?text=${nearestEvent.fields["Title"]}&dates=${eventStartFullYear}${eventStartMonth}${eventStartDay}T${eventStartHour}0000Z/${eventStartFullYear}${eventStartMonth}${eventStartDay}T${eventEndHour}0000Z&details=${nearestEvent.fields["Event Summary"]}&location=https://us02web.zoom.us/j/88292728501?pwd=Y0tBR25GbFF4MUxhWnd5MFI5S3hNUT09&trp=true`
 
   const numDays = Math.floor(timeTillStart / millisecondsInDay) + 1,
     numHours = Math.floor(timeTillStart / millisecondsin1H) + 1,
